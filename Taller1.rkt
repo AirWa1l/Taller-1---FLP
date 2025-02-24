75b86b87b3c0cbbcbedb050047f7e627437f78f4
Jose Adrian Marin <jose.adrian.marin@correounivalle.edu.co>
2025-02-24 11:39:35 -0500

    Añadida función #3

--- Taller1.rkt
+++ Taller1.rkt
@@ -4,3 +4,15 @@
   (lambda ( x y )(if (> x y)(* x y )(- x y )))
  )
 ; Esto solo es una muestra ;
+
+; 3. list-set ;
+(define (list-set L n x)
+  (cond
+    ; Primer caso, sucede cuando le pasamos una lista vacia ;
+    [(eq? L '()) '()]
+    ; Segundo caso, reemplaza el primer elemento cuando n = 0 ;
+    [(= n 0) (cons x(cdr L))]
+    ; Tercer caso, recorre el resto de la lista ;
+    [(> n 0) (cons(car L) (list-set(cdr L) (- n 1) x))]
+    )
+  )

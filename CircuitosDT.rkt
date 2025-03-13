08d58f5744838f74eb8dd8921f212c9dcba6e844
michaelRS2002 <148493262+michaelRS2002@users.noreply.github.com>
2025-03-13 18:13:56 -0500

    Update CircuitosDT.rkt

--- CircuitosDT.rkt
+++ CircuitosDT.rkt
@@ -7,7 +7,7 @@
 ; Definición del tipo de dato circuito
 ; <circuit> ::= '(circuit <gate-list>)
 (define-datatype circuit circuit?
-  (circuito (gate_list gate_list?)))
+  (a-circuit (gate_list gate_list?)))
 
 ; Definición del tipo de dato lista de compuertas
 ; <gate-list> ::= (empty) | (<gate> <gate-list>)
@@ -19,7 +19,7 @@
 ; <gate> ::= '(gate <gate_id> <type> <input_list>)
 ; <gate_id> ::= <symbol>
 (define-datatype gate gate?
-  (compuerta (gate_id symbol?) (gate_type type?) (input_list input_list?)))
+  (a-gate (gate_id symbol?) (gate_type type?) (input_list input_list?)))
 
 ; Definición de tipos de compuertas lógicas
 ; Representa los diferentes tipos de operaciones lógicas que puede realizar una compuerta.
@@ -44,37 +44,37 @@
 
 ; Ejemplo 1: Circuito con una compuerta NOT
 ; A -> (NOT A) -> SALIDA(G1)
-(circuito
+(a-circuit
         (no-empty-gate-list 
-        (compuerta 'G1 (not) (no-empty-gate-ref 'A (empty-input-list))) 
+        (a-gate 'G1 (not) (no-empty-gate-ref 'A (empty-input-list))) 
         (empty-gate-list)))
 
 ; Ejemplo 2: Circuito AND simple
 ; (A AND B) -> SALIDA (G2)         
-(circuito
+(a-circuit
         (no-empty-gate-list 
-        (compuerta 'G2 (and) (no-empty-gate-ref 'A (no-empty-gate-ref 'B (empty-input-list))))  
+        (a-gate 'G2 (and) (no-empty-gate-ref 'A (no-empty-gate-ref 'B (empty-input-list))))  
         (empty-gate-list)))
 ; Ejemplo 3: Combinación de compuertas
 ; (A OR B) -> G3 -> (NOT G3) -> SALIDA (G4)
-(circuito
+(a-circuit
         (no-empty-gate-list 
-        (compuerta 'G3 (or) (no-empty-gate-ref 'A (no-empty-gate-ref 'B (empty-input-list))))  
+        (a-gate 'G3 (or) (no-empty-gate-ref 'A (no-empty-gate-ref 'B (empty-input-list))))  
         (no-empty-gate-list
-        (compuerta 'G4 (not) (no-empty-gate-ref 'G3 (empty-input-list)))
+        (a-gate 'G4 (not) (no-empty-gate-ref 'G3 (empty-input-list)))
         (empty-gate-list))))
 
 ; Ejemplo 4: Implementación de XOR sin compuerta XOR
 ; (A OR B) -> G1
 ; (A AND B) -> G2 -> (NOT G2) -> G3
 ; (G1 AND G3) -> G4 -> SALIDA
-(circuito
+(a-circuit
         (no-empty-gate-list 
-        (compuerta 'G1 (or) (no-empty-gate-ref 'A (no-empty-gate-ref 'B (empty-input-list))))  
+        (a-gate 'G1 (or) (no-empty-gate-ref 'A (no-empty-gate-ref 'B (empty-input-list))))  
         (no-empty-gate-list
-        (compuerta 'G2 (and) (no-empty-gate-ref 'A (no-empty-gate-ref 'B (empty-input-list))))
+        (a-gate 'G2 (and) (no-empty-gate-ref 'A (no-empty-gate-ref 'B (empty-input-list))))
         (no-empty-gate-list
-        (compuerta 'G3 (not) (no-empty-gate-ref 'G2 (empty-input-list)))
+        (a-gate 'G3 (not) (no-empty-gate-ref 'G2 (empty-input-list)))
         (no-empty-gate-list
-        (compuerta 'G4 (and) (no-empty-gate-ref 'G1 (no-empty-gate-ref 'G3 (empty-input-list))))
+        (a-gate 'G4 (and) (no-empty-gate-ref 'G1 (no-empty-gate-ref 'G3 (empty-input-list))))
         (empty-gate-list))))))

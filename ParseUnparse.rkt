f5add853160a983261ca22d31fc4d975ef32e3b1
michaelRS2002 <148493262+michaelRS2002@users.noreply.github.com>
2025-03-13 23:32:29 -0500

    Update ParseUnparse.rkt

--- ParseUnparse.rkt
+++ ParseUnparse.rkt
@@ -6,39 +6,33 @@
 ; de un circuito ;
 
 ; Definición de estructuras de datos ;
+
 ; Definición del tipo de dato circuito
 ; <circuit> ::= '(circuit <gate-list>)
 (define-datatype circuit circuit?
-  (a-circuit (gate_list gate_list?)))
+  (a-circuit (gate_list gate?)))
 
-; Definición del tipo de dato lista de compuertas
-; <gate-list> ::= (empty) | (<gate> <gate-list>)
-(define-datatype gate_list gate_list?
-  (empty-gate-list)
-  (no-empty-gate-list (first gate?) (rest gate_list?)))
-
-; Definición del tipo de dato compuerta lógica
-; <gate> ::= '(gate <gate_id> <type> <input_list>)
-; <gate_id> ::= <symbol>
+; Definición del tipo de dato compuerta lógica ;
+; <gate> ::= '(gate <name> <gate-type> <inputs>) ;
+; <name ::= <symbol>
 (define-datatype gate gate?
-  (a-gate (gate_id symbol?) (gate_type type?) (input_list input_list?)))
+  (a-gate (name symbol?) (type gate-type?) (inputs list?)))
 
-; Definición de tipos de compuertas lógicas
-; Representa los diferentes tipos de operaciones lógicas que puede realizar una compuerta.
-; <type> ::= and | or | not | xor
-(define-datatype type type?
-  (and−type)
-  (or−type)
-  (not−type)
-  (xor−type))
+; Definición del tipo de compuerta lógica ;
+; <gate-type> ::= 'not | 'and | 'or
+(define-datatype gate-type gate-type?
+  (not-type)
+  (and-type)
+  (or-type)
+  (xor-type))
 
-; Definición de lista de entradas (input_list)
-; <input_list> ::= empty | <bool> <input_list> | <gate_ref> <input_list>
-; <gate_ref> ::= <symbol>
-(define-datatype input_list input_list?
-  (empty-input-list)
-  (no-empty-bool-input-list (first boolean?) (rest input_list?))
-  (no-empty-gate-ref (first symbol?) (rest input_list?)))
+; Definición de entradas de las compuertas lógicas ;
+; <a-input> ::= <bool-input> | <ref-input> ;
+; <bool-input> ::= <boolean> ;
+; <ref-input> ::= <symbol>
+(define-datatype a-input a-input?
+  (bool-input (val boolean?))
+  (ref-input (name symbol?)))
 
 ; --------------------------------------------------------------------
 ; Funciones PARSEBNF y UNPARSEBNF ;
@@ -70,7 +64,7 @@
 ; entradas, si g no es válida devolveremos un not-type sin entradas ;
 (define (parse-gate g)
   (if (and (pair? g) (eq? (car g) 'gate))
-      (a-gate (cadr g) (parse-gate-type (caddr g)) (parse-input-list (cadddr g)))
+      (a-gate (cadr g) (parse-gate-type (cadr(caddr g))) (parse-input-list (cadddr g)))
       (a-gate 'unknown (not-type) '())))
 
 ; Aquí traduciremos el tipo de compuerta gt a una estructura que ;
@@ -81,7 +75,10 @@
     [(eq? gt 'not) (not-type)]
     [(eq? gt 'and) (and-type)]
     [(eq? gt 'or) (or-type)]
-    [else (not-type)]))
+    [(eq? gt 'xor) (xor-type)]
+    [else (eopl:error 'parse-gate-type 
+                      "Tipo de operación no válido en la gramática: ~s" gt)]))
+
 
 ; Convertiremos una lista de entradas ilist en la estructura que ;
 ; definimos, en caso de que sea vacía nos devolverá una lista vacia ;
@@ -103,54 +100,3 @@
     [(boolean? inp) (bool-input inp)]
     [(symbol? inp) (ref-input inp)]
     [else (bool-input #f)]))
-; ----------------------------------------- ;
-; UNPARSEBNF
-; ----------------------------------------- ;
-
-; Inicialmente, recibimos una instancia a la que llamaremos circuit- :
-; parse y usamos cases para verificar si este es de tipo a-circuit ;
-; y extraemos el campo gate del circuito y lo convertimos en un lista ;
-; con la estructura circuit compuertas, por último llamamos a ;
-; unparse-gate para convertir la compuerta ;
-(define (UNPARSEBNF circuit-parse)
-  (cases circuit circuit-parse
-    (a-circuit (gate) (list 'circuit (unparse-gate gate)))))
-
-; Aqui sencillamente recibimos la compuerta g y la evaluamos con cases ;
-; verificando que sea de tipo a-gate, si lo es extraemos name, type ;
-; e inputs y devolvemos una lista, llamaos a unparse-gate-type y ;
-; a unparse-input-list para convertir los datos correspondientes ;
-(define (unparse-gate g)
-  (cases gate g
-    (a-gate (name type inputs)
-      (list 'gate
-            name
-            (unparse-gate-type type)
-            (list 'input_list (unparse-input-list inputs))))))
-
-; En esta función recibimos gt que representa el tipo de compuerta ;
-; comparamos y devolvemos o un not, and o or dependiendo del caso ;
-(define (unparse-gate-type gt)
-  (cond
-    [(eq? gt not-type) 'not]
-    [(eq? gt and-type) 'and]
-    [(eq? gt or-type) 'or]
-    [else 'not])) ;; Caso por defecto en caso de error
-
-; Convertiremos una lista de entradas ilist en la estructura que ;
-; definimos, en caso de que sea vacía nos devolverá una lista vacia ;
-; pero si esta tiene elementos, tomamos el 1er elemento y le aplicamos ;
-; unparse-input, se agregar a una nueva lista y llamamos a unparse- ;
-; input-list y hacemos lo mismo con el resto de la lista ;
-(define (unparse-input-list ilist)
-  (if (null? ilist) '()  
-      (cons (unparse-input (car ilist)) 
-            (unparse-input-list (cdr ilist)))))
-
-; Finalmente, recibirmos un inp que es una entrada de la compuerta ;
-; Ysamos los cases para identificar los 2 tipos de entreda y dependdiendo ;
-; del caso devolvemos val o name "
-(define (unparse-input inp)
-  (cases a-input inp
-    (bool-input (val) val)
-    (ref-input (name) name)))
